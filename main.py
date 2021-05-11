0dcc0e80eddc4a5c32053457d0a597495de4645e
saikiran8 <kollojusaikiran@gmail.com>
2021-05-11 20:14:48 +0530

    Added sql connection code

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
 
+mysql = MySQL(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'Flaks_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+conn = mysql.connect()
 @app.route('/')
 def home():
-    return "Hey Saiki"
+    return "Hey "
 @app.route('/saiki')
 def pint():
-    return "Hey Pintu"
-
-
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM flask")
+        fetchdata = cur.fetchall()
+        cur.close()
+    except Exception as e:
+        print(e)
+ #return render_template('home.html',data = fetchdata)
+    return str(fetchdata)
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
