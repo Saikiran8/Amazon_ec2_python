7e99788b90502b7d48149cee7319d74909387aae
saikiran8 <kollojusaikiran@gmail.com>
2021-05-16 22:11:15 +0530

    added Led routes

--- main.py
+++ main.py
@@ -4,24 +4,52 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'cbit1234'
-app.config['MYSQL_DATABASE_DB'] = 'database-1'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Saikiran@86'
+app.config['MYSQL_DATABASE_DB'] = 'users'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 conn = mysql.connect()
 @app.route('/')
 def home():
-    return "Hey "
-@app.route('/saiki')
-def pint():
     try:
         cur = conn.cursor()
-        cur.execute(
+        cur.execute("select userID from user where userName='Temp';")
         fetchdata = cur.fetchall()
         cur.close()
     except Exception as e:
         print(e)
-    return {fetchdata}
+    return '{"2":"'+str(fetchdata)+'"}'
+@app.route('/led1')
+def ledOn():
+    try:
+        cur = conn.cursor()
+        cur.execute("update user set userID=1 where userName='Temp'")
+        #fetchdata = cur.fetchall()
+        print("Led ON")
+        cur.close()
+    except Exception as e:
+        print(e)
+@app.route('/led0')
+def ledOff():
+    try:
+        cur = conn.cursor()
+        cur.execute("update user set userID=0 where userName='Temp'")
+        #fetchdata = cur.fetchall()
+        print("Led OFF")
+        cur.close()
+    except Exception as e:
+        print(e)
+@app.route('/getTemp')
+def pint():
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM user")
+        fetchdata = cur.fetchall()
+        cur.close()
+    except Exception as e:
+        print(e)
+ #return render_template('home.html',data = fetchdata)
+    return str(fetchdata)
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080, debug=False)
